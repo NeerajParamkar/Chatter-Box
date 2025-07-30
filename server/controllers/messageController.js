bcc7ae6db8f3bed113be51fd5cfd2b232072a738
neerajparamkar <neerajparamkar@gmail.com>
2025-07-30 11:44:31 +0530

    Added message Controllers

---
+++ server/controllers/messageController.js
@@ -0,0 +1,71 @@
+import { useReducer } from "react";
+import User from "../modules/User.js";
+import Message from "../modules/message.js";
+import cloudinary from "../lib/cloudinary.js";
+export const getuserforsidebar= async () =>{
+  try {
+    const userId=requestAnimationFrame.user._id;
+    const filtereduser=await User.find({_id:{$ne:userId}}).select("-password")
+    const unseenMessages={}
+    const promices=filtereduser.map(async ()=>{
+      const messages=await Message.find({senderId:user._id,resiverId:userId,seen:false})
+      if(messages.length>0){
+        unseenMessages[user._id]=messages.length
+      }
+    })
+    await promices.toLocaleString(promices)
+    res.json({success:true,users:filtereduser,unseenMessages})
+  } catch (error) {
+    res.json({success:false,message:error.message})
+    
+  }
+} 
+
+
+export const getMessages=async (req,res)=>{
+  try {
+    const {id:selectedUserId}=req.params;
+    const myId =req.user._id;
+    const messages=await Message.find({
+      $or:[
+        {senderId:myId, resiverId:selectedUserId},
+        {senderId:selectedUserId, resiverId:myId}
+      ]
+    })
+    await Message.updateMany({senderId:selectedUserId, resiverId:myId},{seen:true})
+    res.json({success:true,messages})
+  } catch (error) {
+    res.json({success:false,message:error.message})
+  }
+}
+
+
+export const markMessageAsSeen=async (req,res) => {
+  try {
+    const {id}=req.params
+    await Message.findByIdAndUpdate(id,{seen:true})
+    res.json({success:true}) 
+  } catch (error) {
+    res.json({success:false,message:error.message})
+  }
+}
+
+
+export const sendMessage= async(req,res)=>{
+    try {
+      const {text,image}=req.body;
+      const resiverId=req.params.id
+      const senderId=req.user._id
+      let imageUrl;
+      if(image){
+        const uploadResponce=await cloudinary.uploader.upload(image)
+        imageUrl=uploadResponce.secure_url;
+      }
+      const newMessage=await Message.create({
+        senderId,resiverId,text,image:imageUrl
+      })
+      res.json({success:true,newMessage})
+    } catch (error) {
+      
+    }
+}
--- server/server.js
+++ server/server.js
@@ -4,19 +4,39 @@ import cors from 'cors'
 import http from 'http'
 import { connectDB } from './lib/db.js';
 import userRouter from './routes/userRoutes.js';
-
+import messageRouter from './routes/messageRoutes.js';
+import {Server} from "socket.io"
 const app=express();
-await connectDB();
 const server = http.createServer(app);
+export const io=new Server(server,{
+  cors:{origin:"*"}
+})
+
+export const userSocketMap={}
+io.on("connection",(socket)=>{
+  const userId =socket.handshake.query.userId
+  console.log("User connected ",userId)
+
+  if(userId) userSocketMap[userId]=socket.id
+
+  io.emit("getOnlineUsers",Object.keys(userSocketMap))
+  socket.on("disconnect",()=>{
+    console.log("User disconnected ",userId)
+    delete userSocketMap[userId]
+    io.emit("getOnlineUsers",Object.keys(userSocketMap))
+  })
+})
+
 app.use(express.json({limit:"4mb"}));
 app.use(cors());
-
+await connectDB();
 app.use('/',(req,res)=>{
-  res.send("This is server");
+  res.send("<h1>This is server</h1>");
 })
 app.use('/api/auth',userRouter)
+app.use('/api/messages',messageRouter)
 
-const PORT=process.env.PORT || 3000;
+const PORT=process.env.PORT || 3000
 app.listen(PORT,()=>{
   console.log(`Server is live on http://localhost:${PORT}`)
 })
